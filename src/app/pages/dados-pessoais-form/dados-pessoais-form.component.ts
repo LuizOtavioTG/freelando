19fa2e8719d066deb4fe085ecd309a869610ea95
LuizOtavioTG <bc.luizo@gmail.com>
2025-07-07 20:41:40 -0300

    Feat: Aplicando o form dinâmico de dados pessoais na view

--- src/app/pages/dados-pessoais-form/dados-pessoais-form.component.ts
+++ src/app/pages/dados-pessoais-form/dados-pessoais-form.component.ts
@@ -9,6 +9,10 @@ import { Cidade, Estado, IbgeService } from '../../shared/services/ibge.service'
 import { cpfValidator } from '../../shared/validators/cpf.validator';
 import { emailExistenteValidator } from '../../shared/validators/emailExistente.validator';
 import { EmailValidatorService } from '../../shared/services/email-validator.service';
+import { FormConfig } from '../../shared/models/form-config.interface';
+import { DynamicFormService } from '../../shared/services/dynamic-form.service';
+import { getDadosPessoaisConfig } from '../../config/dados-pessoais-form.config';
+import { FormFieldBase } from '../../shared/models/form-field-base.interface';
 
 
 export const senhasIguaisValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
@@ -27,12 +31,14 @@ export const senhasIguaisValidator: ValidatorFn = (control: AbstractControl): Va
     CommonModule,
     ReactiveFormsModule,
     ButtonComponent,
+    
   ],
   templateUrl: './dados-pessoais-form.component.html',
   styleUrls: ['./dados-pessoais-form.component.scss']
 })
 export class DadosPessoaisFormComponent implements OnInit {
   dadosPessoaisForm!: FormGroup;
+  formConfig!: FormConfig;
 
   estado$!: Observable<Estado[]>;
   cidade$!: Observable<Cidade[]>;
@@ -44,22 +50,19 @@ export class DadosPessoaisFormComponent implements OnInit {
     private router: Router,
     private cadastroService: CadastroService,
     private ibgeService: IbgeService,
-    private emailValidatorService: EmailValidatorService
-  ) { }
+    private emailValidatorService: EmailValidatorService,
+    private dynamicFormService: DynamicFormService
+  ) {
+    this.dynamicFormService.registerFormConfig('dadosPessoaisForm', getDadosPessoaisConfig)
+  }
 
   ngOnInit(): void {
-    const formOptions: AbstractControlOptions = {
-      validators: senhasIguaisValidator
-    };
-    this.dadosPessoaisForm = this.fb.group({
-      nomeCompleto: ['', Validators.required],
-      cpf: ['', [Validators.required, cpfValidator]],
-      estado: ['', Validators.required],
-      cidade: [{ value: '', disabled: true }, Validators.required],
-      email: ['', [Validators.required, Validators.email],[emailExistenteValidator(this.emailValidatorService)]],
-      senha: ['', [Validators.required, Validators.minLength(6)]],
-      confirmaSenha: ['', Validators.required]
-    }, formOptions);
+    this.formConfig = this.dynamicFormService.getFormConfig('dadosPessoais', this.emailValidatorService);
+
+    this.dadosPessoaisForm = this.dynamicFormService.createFormGroup(
+      this.formConfig,
+      { validators: senhasIguaisValidator }
+    );
 
     this.carregarEstados();
     this.configurarListenerEstado();
@@ -67,16 +70,6 @@ export class DadosPessoaisFormComponent implements OnInit {
   onAnterior(): void {
     this.salvarDadosAtuais();
     this.router.navigate(['/cadastro/area-atuacao'])
-  }
-  salvarDadosAtuais() {
-    const formValue = this.dadosPessoaisForm.value;
-    this.cadastroService.updateCadastroData({
-      nomeCompleto: formValue.nomeCompleto,
-      estado: formValue.estado,
-      cidade: formValue.cidade,
-      email: formValue.email,
-      senha: formValue.senha
-    })
   }
 
   onProximo(): void {
@@ -88,38 +81,61 @@ export class DadosPessoaisFormComponent implements OnInit {
     }
   }
 
+  salvarDadosAtuais() {
+    const formValue = this.dadosPessoaisForm.value;
+    this.cadastroService.updateCadastroData({
+      nomeCompleto: formValue.nomeCompleto,
+      estado: formValue.estado,
+      cidade: formValue.cidade,
+      email: formValue.email,
+      senha: formValue.senha
+    })
+  }
+
+  isFieldType(field: FormFieldBase, type: string): boolean {
+    return field.type === type;
+  }
+
+  hasField(name: string): boolean {
+    return this.formConfig.fields.some(field => field.formControlName === name);
+  }
+
+  getFieldByName(name: string): FormFieldBase {
+    return this.formConfig.fields.find(field => field.formControlName === name) || {} as FormFieldBase;
+  }
+
   private carregarEstados(): void {
     this.estado$ = this.ibgeService.getEstados();
   }
 
   private configurarListenerEstado(): void {
-  const estadoControl = this.dadosPessoaisForm.get('estado');
-  const cidadeControl = this.dadosPessoaisForm.get('cidade');
+    const estadoControl = this.dadosPessoaisForm.get('estado');
+    const cidadeControl = this.dadosPessoaisForm.get('cidade');
 
-  if (estadoControl && cidadeControl) {
-    this.cidade$ = estadoControl.valueChanges.pipe(
-      startWith(''),
-      tap(() => {
-        this.resetarCidade();
-        cidadeControl.disable();
-        this.carregandoCidades$.next(true);
-      }),
-      switchMap(uf => {
-        if (uf) {
-          return this.ibgeService.getCidadesPorEstado(uf).pipe(
-            tap(() => {
-              cidadeControl.enable();
-              this.carregandoCidades$.next(false);
-            })
-          );
-        }
+    if (estadoControl && cidadeControl) {
+      this.cidade$ = estadoControl.valueChanges.pipe(
+        startWith(''),
+        tap(() => {
+          this.resetarCidade();
+          cidadeControl.disable();
+          this.carregandoCidades$.next(true);
+        }),
+        switchMap(uf => {
+          if (uf) {
+            return this.ibgeService.getCidadesPorEstado(uf).pipe(
+              tap(() => {
+                cidadeControl.enable();
+                this.carregandoCidades$.next(false);
+              })
+            );
+          }
 
-        this.carregandoCidades$.next(false);
-        return of([]);
-      })
-    );
+          this.carregandoCidades$.next(false);
+          return of([]);
+        })
+      );
+    }
   }
-}
 
   private resetarCidade(): void {
     this.dadosPessoaisForm.get('cidade')?.setValue('');
